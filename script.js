1b09e73095a2d39cb4a047278cb8672ab45de031
unknown <andreevasian95@gmail.com>
2023-05-19 15:51:11 +0300

    change script file

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-"use strict";
+money = 20000;  // Месячный бюджет
+profit = '5000';  // Фриланс
+expenses = '4000, 2000, 500';  // Расходы
+purpose = 40000;  // Цель зарабоать
+period = 3;  // Период?
+budgetDay = money / 30;  // Дневной доход
+console.log(typeof(money));
+console.log(typeof(profit));
+console.log(expenses.length);
+console.log(`Период равен ${period} месяцев`);
+console.log(`Цель заработать ${purpose} хуёллеров`);
+console.log(Math.round(budgetDay));
