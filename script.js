3d8c0e92a3e26cd772997bba8d87c32a0ae230cc
unknown <andreevasian95@gmail.com>
2023-05-19 20:22:19 +0300

    add massive

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict";
 let money = 20000;  // Месячный бюджет
 let profit = '5000';  // Фриланс
-let expenses = 'Питание: 4000, Проезд: 2000, По приколу: 500';  // Расходы
+let expenses = 'Питание: 4000, Проезд: 2000, По-приколу: 500';  // Расходы
 let purpose = 40000;  // Цель зарабоать
 let period = 3;  // Период?
 let budgetDay = money / 30;  // Дневной доход
@@ -12,6 +12,12 @@ console.log(`Период равен ${period} месяцев`);
 console.log(`Цель заработать ${purpose} хуёллеров`);
 console.log(Math.round(budgetDay));
 expenses = expenses.toLowerCase();  // Приведение к нижниму регистру
-for (const symbol of expenses) {
-    console.log(symbol);
+expenses = expenses.split(" ");
+console.log(expenses);
+var a = '';
+for (let i = 0; i < expenses.length; i++) {
+    if (i % 2 == 0 ) {
+        a += expenses[i];
+    }
 }
+console.log(a);
